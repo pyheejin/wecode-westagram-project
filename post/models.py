4fca8707896b2de5b3d164ebb18cdff047f33a6b
pyjjin <pyheejin@gmail.com>
2020-04-06 20:05:04 +0900

    api 수정

--- post/models.py
+++ post/models.py
@@ -5,7 +5,7 @@ from user.models import User
 
 
 class Comment(models.Model):
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     text = models.CharField(max_length=300)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CommentView
+from .views import CommentView, myCommentView
 
 urlpatterns = [
-    path('comments/', CommentView.as_view()),
+    path('comments/', CommentView.as_view(), name='comment'),
+    path('<str:name>/', myCommentView.as_view(), name='mycomment'),
 ]
